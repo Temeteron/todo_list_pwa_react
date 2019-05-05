9bbf2f304a8ef13abe655e31e11d87a67facdf45
Dimitris Greasidis <dgreasidis@gmail.com>
2019-05-05 19:27:29 +0300

    deploy

---
+++ precache-manifest.a935d718eea4e463938a388bc3354859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea7cf56da016393d371e",
+    "url": "/todo_list_pwa_react/static/js/runtime~main.ea7cf56d.js"
+  },
+  {
+    "revision": "e3d6a8b672c3d82508bf",
+    "url": "/todo_list_pwa_react/static/js/main.e3d6a8b6.chunk.js"
+  },
+  {
+    "revision": "2f2c191c6a09f80962fb",
+    "url": "/todo_list_pwa_react/static/js/1.2f2c191c.chunk.js"
+  },
+  {
+    "revision": "e3d6a8b672c3d82508bf",
+    "url": "/todo_list_pwa_react/static/css/main.3a45e50d.chunk.css"
+  },
+  {
+    "revision": "1158350e75bfb0312d6bf7475488ea3a",
+    "url": "/todo_list_pwa_react/index.html"
+  }
+];
