27a1c227089dfa11a79c1186e562a7e37ba92ed4
Dimitris Greasidis <dgreasidis@gmail.com>
2019-05-05 18:56:17 +0300

    add docs folder for gh-pages

---
+++ docs/precache-manifest.caebe6943cec84ff0d0e2bac8431ac36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6deadd975bc0804b642e",
+    "url": "/static/js/main.6deadd97.chunk.js"
+  },
+  {
+    "revision": "a87c11204f80043de096",
+    "url": "/static/js/1.a87c1120.chunk.js"
+  },
+  {
+    "revision": "6deadd975bc0804b642e",
+    "url": "/static/css/main.3a45e50d.chunk.css"
+  },
+  {
+    "revision": "590ec87c7b1633f93c6d1cc9e9cd3e6e",
+    "url": "/index.html"
+  }
+];
